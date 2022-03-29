418a5e1b778f67fd6a529fccee51965d52e599b1
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-28 21:15:22 -0300

    Exemplo cont++

---
+++ ExemploWhileContador.java
@@ -0,0 +1,9 @@
+public class ExemploWhileContador {
+    public static void main(String[] args) {
+        int cont = 1;
+        while(cont++ <= 3){
+            System.out.println(cont);
+        }
+        System.out.println(cont);
+    }
+}
