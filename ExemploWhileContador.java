1060166cf470aed5d9324e9ed78b1a4fed023d6b
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-28 21:23:46 -0300

    cont++ no print

--- ExemploWhileContador.java
+++ ExemploWhileContador.java
@@ -1,9 +1,16 @@
 public class ExemploWhileContador {
     public static void main(String[] args) {
         int cont = 1;
-        while(cont++ <= 3){
-            System.out.println(cont);
+        while (cont <= 2){
+            System.out.println(cont++);
         }
-        System.out.println(cont);
+        // while (++cont <= 3){
+        //     System.out.println(cont);
+        // }
+        // System.out.println(cont);
+        // while(cont++ <= 3){
+        //     System.out.println(cont);
+        // }
+        // System.out.println(cont);
     }
 }
