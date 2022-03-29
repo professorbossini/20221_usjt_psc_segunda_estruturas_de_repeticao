240f76491b5776d4adcda2001d6340e9c1cafeb2
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-28 21:43:49 -0300

    Traduzindo um while para um for

--- ExemploFor.java
+++ ExemploFor.java
@@ -1,5 +1,15 @@
 public class ExemploFor {
     public static void main(String[] args) {
-        
+        int cont;
+        for(cont = 1;cont <= 2;cont++){
+            System.out.println(cont);
+        }
+        System.out.println(cont);
+
+        // int cont = 1;
+        // while (cont <= 10){
+        //     System.out.println(cont);
+        //     cont++;
+        // }
     }
 }
