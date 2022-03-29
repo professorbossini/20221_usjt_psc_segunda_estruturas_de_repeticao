9ef57bd406c0ee8fb58c562ac73f6a674262318a
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-28 21:31:07 -0300

    Comecando for

---
+++ ExemploFor.java
@@ -0,0 +1,5 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        
+    }
+}
--- ExemploWhileContador.java
+++ ExemploWhileContador.java
@@ -1,10 +1,15 @@
 public class ExemploWhileContador {
     public static void main(String[] args) {
         int cont = 1;
-        while (cont <= 2){
-            System.out.println(++cont);
+        while (cont <= 10){
+            System.out.println(cont);
+            cont++;
         }
-        
+        // int cont = 1;
+        // while (cont <= 2){
+        //     System.out.println(++cont);
+        // }
+
         // while (++cont <= 3){
         //     System.out.println(cont);
         // }
